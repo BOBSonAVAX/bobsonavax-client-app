3d923d8322200f8006ef723dd4836c0364733a70
irobertcostin <irobertcostin@gmail.com>
2024-01-07 12:31:50 +0100

    setup the image on landpage

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,35 +1,22 @@
-import { useEffect } from "react";
-import { LP_CONTRACT } from "../../contracts/Addresses"
-import { Web3Button, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
+import { useEffect, useState } from "react";
+import landpagepic from "../images/bobs_app_landpage.png"
 
 
 
 const Home: React.FC = () => {
 
-    const address = useAddress();
-
-    const { contract } = useContract(LP_CONTRACT)
-
-    // const {
-    //     data: contractSwap,
-    //     isLoading: isContractLoading
-    // } = useContractRead(contract, "approve")
-
-
 
 
     useEffect(() => {
-        console.log(contract);
-        if (address) {
-            console.log(address);
-        }
 
-    }, [address])
+    }, [])
 
     return (
         <div className=" flex justify-end">
 
-            <div>
+            <div className="w-full flex flex-col gap-10 justify-center items-center pl-5 sm:pl-0 mt-10 overflow-hidden">
+                <img src={landpagepic} className="w-full min-w-[500px]"></img>
+
 
             </div>
 
