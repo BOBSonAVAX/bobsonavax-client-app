825c260ef1330f5f4048656a6ef3e2d1fce6a8da
Anthony Bautista <anthonya.bautista@gmail.com>
2024-01-22 11:33:50 -0600

    add missing components

---
+++ src/components/Empty.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Empty: React.FC = () => {
+
+
+    return (
+        <div className="w-full lg:text-xl text-center" >
+            <p>Not Connected</p>
+        </div>
+    )
+
+}
+
+
+export default Empty;
